ce9bc6e5f05e863f58694d31ff2b186636af9bfa
biangacila <biangacila@gmail.com>
2022-11-26 13:24:28 +0200

    update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import './assets/styles/App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App(){
+
+    let name="Bruno"
+     function getCountry(){
+        return "CONGO"
+     }
+     function clc(num1,num2){
+        return num1+num2
+     }
+
+    return(
+        <div>
+            <h1 className={"title"}>Main app</h1>
+            <p>Hi ! brother {name} from {getCountry()}</p>
+            <p>Some of 2 + 6 = {clc(2,6)}</p>
+        </div>
+    )
 }
 
-export default App;
